19572ad9b7af1a55d338d2c0dd199d3f6ccea837
Methusela11 <soenkilifi.com>
2026-03-02 13:47:12 +0300

    Fixing db error

--- kentainerss/settings.py
+++ kentainerss/settings.py
@@ -96,12 +96,19 @@ WSGI_APPLICATION = 'kentainerss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-import os
-
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+# Use DATABASE_URL if set (production), otherwise use SQLite locally
+if os.environ.get("DATABASE_URL"):
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ["DATABASE_URL"])
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
